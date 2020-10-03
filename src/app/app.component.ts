39a25e76a7c3990caa3a30290968edf67989149f
Sriganesh Lokesh <sriganeshlokesh@Sriganeshs-MacBook-Pro.local>
2020-10-03 19:03:35 -0400

    Angular Crash Course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-crash-course';
+  name: string = 'Ganesh';
+
+  constructor() {
+    this.changeName('Brad Traversy');
+  }
+
+  changeName(name: string) {
+    this.name = name;
+  }
 }
---
+++ src/app/models/Todo.ts
@@ -0,0 +1,8 @@
+export class Todo {
+  // if you want it to be optional
+  // add a ?
+  // id?:number
+  id: number;
+  title: string;
+  completed: boolean;
+}
